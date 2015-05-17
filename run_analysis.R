446dde82d4b2c9873a06b4b1c62ea53f01225b2b
Leo Arias <yo@elopio.net>
2015-05-17 03:40:51 -0600

    Merged the test and train datasets.

--- run_analysis.R
+++ run_analysis.R
@@ -13,6 +13,37 @@
 # You should have received a copy of the GNU General Public License
 # along with datasciencecoursera.  If not, see <http://www.gnu.org/licenses/>.
 
+MergeTrainAndTestSets <- function() {
+  # Merges the tran and test data sets from the Samsung data directory.
+  #
+  # If the data is not present in the working directory, this function
+  # downloads and extracts the original data set.
+  #
+  # Returns:
+  #  A data table with the merged data set.
+  GetData()
+  # Read all the original data sets.
+  dataset.dir <- "UCI HAR Dataset"
+  activity.labels <- ReadDataTable(dataset.dir, "activity_labels.txt")
+  features <- ReadDataTable(dataset.dir, "features.txt")
+  test.dataset.dir <- paste(dataset.dir, "test", sep="/")
+  test.subjects <- ReadDataTable(test.dataset.dir, "subject_test.txt")
+  test.measurements <- ReadDataTable(test.dataset.dir, "X_test.txt")
+  test.activities <- ReadDataTable(test.dataset.dir, "y_test.txt")
+  train.dataset.dir <- paste(dataset.dir, "train", sep="/")
+  train.subjects <- ReadDataTable(train.dataset.dir, "subject_train.txt")
+  train.measurements <- ReadDataTable(train.dataset.dir, "X_train.txt")
+  train.activities <- ReadDataTable(train.dataset.dir, "y_train.txt")
+
+  # Merge the test data set.
+  test.dataset <- cbind(test.subjects, test.activities, test.measurements)
+  # Merge the train data set.
+  train.dataset <- cbind(train.subjects, train.activities, train.measurements)
+
+  # Join the test and train data sets.
+  dataset <- rbind(train.dataset, test.dataset)
+}
+
 GetData <- function() {
   # Gets the Samsung accelerometers data required for the project.
   # If the data directory already exists, it does nothing.
@@ -37,29 +68,6 @@ DownloadAndExtractData <- function() {
     download.file(file.url, destfile=file.name, method="curl")
   }
   unzip(file.name)
-}
-
-MergeTrainAndTestSets <- function() {
-  # Merges the tran and test data sets from the Samsung data directory.
-  #
-  # If the data is not present in the working directory, this function
-  # downloads and extracts the original data set.
-  #
-  # Returns:
-  #  A data table with the merged data set.
-  GetData()
-  # Read all the original data sets.
-  dataset.dir <- "UCI HAR Dataset"
-  activity.labels <- ReadDataTable(dataset.dir, "activity_labels.txt")
-  features <- ReadDataTable(dataset.dir, "features.txt")
-  test.dataset.dir <- paste(dataset.dir, "test", sep="/")
-  test.subjects <- ReadDataTable(test.dataset.dir, "subject_test.txt")
-  x.test <- ReadDataTable(test.dataset.dir, "X_test.txt")
-  y.test <- ReadDataTable(test.dataset.dir, "y_test.txt")
-  train.dataset.dir <- paste(dataset.dir, "train", sep="/")
-  train.subjects <- ReadDataTable(train.dataset.dir, "subject_train.txt")
-  x.train <- ReadDataTable(train.dataset.dir, "X_train.txt")
-  y.train <- ReadDataTable(train.dataset.dir, "y_train.txt")
 }
 
 ReadDataTable <- function(directory, file.name) {
